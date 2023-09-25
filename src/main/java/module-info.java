ca6d845992e083bc44c53b0c25f95e8ff6df6f03
mashezhang <mashezhang@foxmail.com>
2023-09-25 10:41:29 +0800

    refactor：删除教程代码

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,5 @@
 module com.mx.fxlesson {
     requires javafx.controls;
-    requires javafx.fxml;
-
     requires com.dlsc.formsfx;
-
-    opens com.mx.fxlesson to javafx.fxml;
     exports com.mx.fxlesson;
 }
